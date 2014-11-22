83438c4c6b3f5d3f6eda4d074581683c3fc85ead
zc <coder.chenzhi@gmail.com>
2014-11-22 09:59:04 +0800

    change code structure

---
+++ TestPython/src/Network/TestSocketClient.py
@@ -0,0 +1,16 @@
+''' 
+Created on 14, October, 2014
+ 
+@author: chenzhi
+'''
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 9999))
+print s.recv(1024)
+for data in ['Michael', 'Tracy', 'Sarah']:
+    s.send(data)
+    print s.recv(1024)
+s.send('exit')
+s.close()
